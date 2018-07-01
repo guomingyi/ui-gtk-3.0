180ff44b03c3d2901c83f32ad5a0121a4f801fae
gmy <296778322@qq.com>
2018-07-01 20:52:40 +0800

    <libusb cross build demo>

--- test/main.c
+++ test/main.c
@@ -1,4 +1,7 @@
 #include <gtk/gtk.h>
+#include "libusb.h"
+
+int list_usb_devs(void);
 
 static void activate (GtkApplication* app,
           gpointer        user_data)
@@ -11,17 +14,22 @@ static void activate (GtkApplication* app,
   gtk_widget_show_all (window);//界面显示show（类Qt::widget->show()） interfaces_loop
 }
  
-int
-main (int    argc,
-      char **argv)
+int main (int argc, char **argv)
 {
   GtkApplication *app;    //App应用入口
 int status;
+int ret;
+
+ret = list_usb_devs();
+printf("%s:list_usb_devs:%d\n",ret);
+
 app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
 //G_APPLICATION_FLAGS_NONE这个暂时还不知道是个什么样的宏  
 g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);//信号回调函数
 status = g_application_run (G_APPLICATION (app), argc, argv);//终端调用参数传入
 g_object_unref (app);
+
+
 return status;
 }
 
