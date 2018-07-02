27d8b23168d49a94952476ed14a3477c87ef99e6
gmy <296778322@qq.com>
2018-07-02 12:04:09 +0800

    <ui layout init>

--- test/main.c
+++ test/main.c
@@ -1,45 +1,86 @@
+#include <stdio.h>
 #include <gtk/gtk.h>
-#include "libusb.h"
 
-int list_usb_devs(void);
-
-static void activate (GtkApplication* app,
-          gpointer        user_data)
+static void helloWorld(GtkWidget *wid, GtkWidget *win)
 {
-  GtkWidget *window;//窗口类型指针
- 
-  window = gtk_application_window_new (app);//界面入口函数
-  gtk_window_set_title (GTK_WINDOW (window), "Window");//window窗口标题设置
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);//window大小设置
-  gtk_widget_show_all (window);//界面显示show（类Qt::widget->show()） interfaces_loop
-}
- 
-int main (int argc, char **argv)
-{
-  GtkApplication *app;    //App应用入口
-int status;
-int ret;
+    GtkWidget *dialog = NULL;
 
-ret = list_usb_devs();
-printf("%s:list_usb_devs:%d\n",ret);
-
-app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-//G_APPLICATION_FLAGS_NONE这个暂时还不知道是个什么样的宏  
-g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);//信号回调函数
-status = g_application_run (G_APPLICATION (app), argc, argv);//终端调用参数传入
-g_object_unref (app);
-
-
-return status;
+    dialog = gtk_message_dialog_new(GTK_WINDOW (win), GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_CLOSE, "Hello World!");
+    gtk_window_set_position(GTK_WINDOW (dialog), GTK_WIN_POS_CENTER);
+    gtk_dialog_run(GTK_DIALOG (dialog));
+    gtk_widget_destroy(dialog);
 }
 
+int main(int argc,char *argv[])
+{
+    static GdkColor red = {0, 0xffff, 0, 0};
+    static GdkColor green = {0, 0, 0xffff, 0};
+    static GdkColor blue = {0, 0, 0, 0xffff};
+    static GdkColor yellow = {0, 0xffff, 0xffff, 0};
+    static GdkColor cyan = {0, 0, 0xffff, 0xffff};
 
+    static GdkColor grey51;//grid
+    gdk_color_parse("grey51", &grey51);
 
+    static GdkColor grey81;//item
+    gdk_color_parse("grey51", &grey81);
 
+    static GdkColor LightSkyBlue2;
+    gdk_color_parse("LightSkyBlue2", &LightSkyBlue2);
 
+    gtk_init(&argc,&argv);
 
+    GtkBuilder *builder = gtk_builder_new();
 
+    if (!gtk_builder_add_from_file(builder,"builder.ui", NULL)) {
+        printf("connot load glade file!");
+		return -1;
+    }
+	printf("load glade file success");
 
+    GtkWidget *window = GTK_WIDGET(gtk_builder_get_object(builder,"window1"));
 
+    GtkButton *button_start = GTK_BUTTON(gtk_builder_get_object(builder, "button_start"));
 
+    GtkLabel *label_factory_test = GTK_LABEL(gtk_builder_get_object(builder, "label_factory_test"));
+    //gtk_label_set_text(label, "factory test");
+    gtk_widget_modify_bg(label_factory_test,GTK_STATE_NORMAL, &green);
 
+    GtkLabel *label_title1 = GTK_LABEL(gtk_builder_get_object(builder, "label_title1"));
+    gtk_widget_modify_bg(label_title1,GTK_STATE_NORMAL, &LightSkyBlue2);
+
+    GtkLabel *label_title2 = GTK_LABEL(gtk_builder_get_object(builder, "label_title2"));
+    gtk_widget_modify_bg(label_title2,GTK_STATE_NORMAL, &LightSkyBlue2);
+
+    GtkGrid *gride_test_items = GTK_GRID(gtk_builder_get_object(builder, "grid_test_items"));
+    gtk_widget_modify_bg(gride_test_items,GTK_STATE_NORMAL, &grey51);
+
+    GtkLabel *label_test_item1 = GTK_LABEL(gtk_builder_get_object(builder, "label_test_item1"));
+    gtk_widget_modify_bg(label_test_item1,GTK_STATE_NORMAL, &grey81);
+
+    GtkLabel *label_result1 = GTK_LABEL(gtk_builder_get_object(builder, "label_result1"));
+    // gtk_widget_modify_font(label_result1,GTK_STATE_NORMAL, &grey81);
+    gtk_label_set_markup(GTK_LABEL(label_result1),"<span foreground='red'>result1</span>");
+
+    GtkLabel *label_result2 = GTK_LABEL(gtk_builder_get_object(builder, "label_result2"));
+    gtk_label_set_markup(GTK_LABEL(label_result2),"<span foreground='red'>result2</span>");
+
+    GtkLabel *label_result3 = GTK_LABEL(gtk_builder_get_object(builder, "label_result3"));
+    gtk_label_set_markup(GTK_LABEL(label_result3),"<span foreground='red'>result3</span>");
+
+    GtkLabel *label_result4 = GTK_LABEL(gtk_builder_get_object(builder, "label_result4"));
+    gtk_label_set_markup(GTK_LABEL(label_result4),"<span foreground='red'>result4</span>");
+
+    GtkLabel *label_result5 = GTK_LABEL(gtk_builder_get_object(builder, "label_result5"));
+    gtk_label_set_markup(GTK_LABEL(label_result5),"<span foreground='red'>result5</span>");
+
+    g_signal_connect (button_start, "clicked", helloWorld, (gpointer) window);
+    //g_signal_connect (button, "clicked", gtk_main_quit, NULL);
+
+    const char *text = gtk_button_get_label(button_start);
+    printf("text=%s\n", text);
+
+    gtk_widget_show_all(window);
+    gtk_main();
+    return 0;
+}
