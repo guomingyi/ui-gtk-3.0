dcd977379db41b27600f51485c7faf14f912b8c5
gmy <296778322@qq.com>
2018-08-05 12:09:09 +0800

    linux compile env

--- ftm/init.sh
+++ ftm/init.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# linux 
+sudo apt-get install gtk+-3.0
+sudo apt-get install tree
+
+
+# windows
 mydir=$PWD
-
 cd $mydir/libs/windows/gtk+-bundle_3.10.4-20131202_win64/
-
 find -name '*.pc' | while read pc; do sed -e "s@^prefix=.*@prefix=$PWD@" -i "$pc"; done
-
 cd $mydir
 
 
